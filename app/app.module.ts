f7f8bcf6eef119e796afd073867972a016311f91
sumedh <sumedhsontakke29@gmail.com>
2018-02-11 18:52:33 +0530

    first commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
+//import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { SideBarToggleComponent } from './side-bar-toggle/side-bar-toggle.component';
-
+//import { ComponentLoaderFactory } from 'ng2-bootstrap/component-loader';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ModalModule.forRoot()],
   declarations: [ AppComponent, SideBarComponent, SideBarToggleComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  //providers: 	[ BsModalService, BsModalRef, ComponentLoaderFactory]
 })
 export class AppModule { }
--- app/side-bar/side-bar.component.ts
+++ app/side-bar/side-bar.component.ts
@@ -1,5 +1,7 @@
-import { Component, HostBinding } from '@angular/core';
-
+import { Component, HostBinding, TemplateRef  } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import $ from "jquery";
 @Component({
   selector: './app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -9,12 +11,18 @@ export class SideBarComponent {
 
   @HostBinding('class.is-open')
   isOpen = false;
-
+  modalRef: BsModalRef;	
+ constructor(private modalService: BsModalService) {} 
   toggle() {
     this.isOpen = !this.isOpen;
+	console.log($(".btn-primary").click());
   }
   
   launched(){
   alert()}
+  
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }  
 
 }
